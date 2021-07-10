99ce09badc0afb48f47f104480224e85a9de16d1
Matías <barmat520@gmail.com>
2021-07-10 00:51:43 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,13 @@
+let respuesta = prompt("hola")
+
+if (respuesta == "saludos") {
+	alert("que educado que sos")
+}
+
+else if (respuesta == "chau") {
+	alert("fua que forro")
+}
+
+else {
+	alert("so' mudo vo'?")
+}
